500dc17d168e433b8f03bf53f59290cb1b72746d
Kyle Vaughn <kvaughn2@uwm.com>
2021-08-25 15:56:57 -0400

    Add project files.

---
+++ Chungventory/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chungventory.MvcApplication" Language="C#" %>
---
+++ Chungventory/Models/InventoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Chungventory.Models
+{
+    public class InventoryViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal price { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string ItemPictureFileName { get; set; }
+    }
+}
